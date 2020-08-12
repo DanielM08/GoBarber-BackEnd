325b509cbc16fcef693f9584eca59b15cc0d9419
DanielM08 <danielmarx08@gmail.com>
2020-08-12 19:46:45 -0300

    Update: add user_id to appointments

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -6,17 +6,19 @@ import CreateAppointmentsService from '@modules/appointments/services/CreateAppo
 
 export default class AppointmentsController{
     public async create(request: Request, response: Response): Promise<Response>{
+      const user_id = request.user.id;
       const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+      const parsedDate = parseISO(date);
 
-    const createAppointment = container.resolve(CreateAppointmentsService);
+      const createAppointment = container.resolve(CreateAppointmentsService);
 
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    })
+      const appointment = await createAppointment.execute({
+        date: parsedDate,
+        provider_id,
+        user_id,
+      })
 
-    return response.json(appointment);
-  }
+      return response.json(appointment);
+    }
 }
