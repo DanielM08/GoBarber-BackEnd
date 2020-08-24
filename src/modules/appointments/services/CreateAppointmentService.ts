cbb8cca9b6885b8308fc03adcee69383a4b7c642
DanielM08 <danielmarx08@gmail.com>
2020-08-24 19:59:25 -0300

    Add provider_id to findByDate method on IAppointmentsRepository

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -49,6 +49,7 @@ export default class CreateAppointmentService{
 
     const findAppointmentsInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if(findAppointmentsInSameDate){
